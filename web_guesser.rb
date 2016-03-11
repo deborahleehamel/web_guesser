01d6553219cee5e7c06072070ceda76c48409611
Deborah Hamel <deborahleehamel@gmail.com>
2016-03-11 11:57:28 -0700

    Color extention works.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,11 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+
 # get '/' do
 #   "Hello, World!"
 # end
-secret_number = rand(10)
+NUMBER = rand(10)
+bg_color = "FFFFFF"
 
 get '/' do
-  "The SECRET NUMBER is #{secret_number}"
+  guess = params["guess"]
+  bg_color, message = check_guess(guess)
+  erb :index, :locals => {:number => NUMBER, :message => message, :bg_color => bg_color}
+end
+
+
+def check_guess(guess)
+  if params["guess"].to_i > NUMBER + 5
+    ["FF0000", "Way too high!"]
+  elsif params["guess"].to_i > NUMBER
+    ["F5A9A9", "Too high!"]
+  elsif params["guess"].to_i < NUMBER - 5
+    ["FF0000","Way too low!"]
+  elsif params["guess"].to_i < NUMBER
+    ["F5A9A9", "Too low!"]
+  else
+    ["01DF01", "You got it right!"]
+  end
 end
